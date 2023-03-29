4bd7a9665b96e76eb884e441e84c1bac340d74d9
Israel Oliveira <35226183+raedman90@users.noreply.github.com>
2023-03-29 16:39:49 -0300

    Atividade

---
+++ BMExercicio04/BMExercicio04.java
@@ -0,0 +1,15 @@
+
+/**
+ * Escreva uma descrição da classe BMExercicio04 aqui.
+ * 
+ * @author (seu nome) 
+ * @version (um número da versão ou uma data)
+ */
+public class BMExercicio04
+{
+     public static void main(String[] args){
+        long long1 = 100;
+        int int1 = 200;
+        /*int int2 = int1 + long1; */
+    }
+}
---
+++ CBBAtividadePratica2/CBBAtividadePratica2.java
@@ -0,0 +1,12 @@
+public class CBBAtividadePratica2{
+    public static void main(String[] args){
+        int num1 = 2;
+        int num2 = 5;
+        int num3 = 10;
+        
+        int soma = num1 + num2 + num3;
+        float media = soma/3;
+        System.out.print("A soma é: " + soma +"\n" + "A média é: " + media);
+        
+    }
+}
